e31112694de97196d071c799308093d344d7c9e3
William Dolorito <wdolorito@gmail.com>
2018-05-31 18:33:01 -0700

    Added assets   Reference knit chart symbols   bootstrap archive   jQuery   popper
    Added skeleton files

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const app = express()
+const server = require('http').createServer(app)
+const io = require('socket.io')(server)
+const router = express.Router()
+const views = __dirname + '/views'
+const bodyParser = require('body-parser')
+const port = 3000
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.static(__dirname + '/static'))
+
+router.use(function (req, res, next) {
+  console.log('/' + req.method)
+  next()
+})
+
+router.get('/', function(req, res) {
+  res.sendFile(views + '/index.html')
+})
+
+app.use('/', router)
+
+app.use('*', function(req,res) {
+  res.sendFile(views + '/404.html')
+})
+
+server.listen(port, function() {
+  console.log('Live at Port ' + port)
+})
