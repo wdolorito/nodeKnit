9c65ab243d63230117aa01b4c25294d1354cdcc6
William Dolorito <wdolorito@gmail.com>
2018-06-09 01:35:36 -0700

    Moved routes to separate file

--- index.js
+++ index.js
@@ -2,32 +2,18 @@ const express = require('express')
 const app = express()
 const server = require('http').createServer(app)
 const io = require('socket.io')(server)
-const router = express.Router()
-const views = __dirname + '/views'
 const bodyParser = require('body-parser')
 const port = 3000
+const router = require('./routes')
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static(__dirname + '/static'))
 
-router.use(function (req, res, next) {
-  console.log('/' + req.method)
-  next()
-})
-
-router.get('/', function(req, res) {
-  res.sendFile(views + '/index.html')
-})
-
-router.get('/chart', function(req, res) {
-  res.sendFile(views + '/chart.html')
-})
-
 app.use('/', router)
 
 app.use('*', function(req,res) {
-  res.sendFile(views + '/404.html')
+  res.sendFile(__dirname +  '/views/404.html')
 })
 
 server.listen(port, function() {
