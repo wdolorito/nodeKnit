d55dd8dfd415913ffd1dfd38cbc524756623da3d
William Dolorito <wdolorito@gmail.com>
2018-06-12 22:56:22 -0700

    Added functions for add-row and rem-row Recheck/readd socket.io if necessary

--- static/js/pattern.js
+++ static/js/pattern.js
@@ -1,4 +1,44 @@
 $(document).ready(function() {
+  const blank = '                        <td><div class="box knit"></div></td>'
+  const blankRow = '  <div class="row">\n'
+                 + '    <div class="col">\n'
+                 + '      <div class="pattern-area">\n'
+                 + '          <table class="pattern-table">\n'
+                 + '            <tbody>\n'
+                 + '              <tr>\n'
+                 + '                <th>\n'
+                 + '                  <div class="row-info">Row</div>\n'
+                 + '                  <input id="blank-spinner" class="row-info form-control" type="number" min="0" value="10">\n'
+                 + '                </th>\n'
+                 + '                <td><div class="box knit"></div></td>\n'
+                 + '                <td><div class="box knit"></div></td>\n'
+                 + '                <td><div class="box knit"></div></td>\n'
+                 + '                <td><div class="box knit"></div></td>\n'
+                 + '                <td><div class="box knit"></div></td>\n'
+                 + '                <td><div class="box knit"></div></td>\n'
+                 + '                <td><div class="box knit"></div></td>\n'
+                 + '                <td><div class="box knit"></div></td>\n'
+                 + '                <td><div class="box knit"></div></td>\n'
+                 + '                <td><div class="box knit"></div></td>\n'
+                 + '              </tr>\n'
+                 + '            </tbody>\n'
+                 + '          </table>\n'
+                 + '        </div>\n'
+                 + '      </div>\n'
+                 + '    </div>\n'
+
+  $('#add-row').on('click', function(){
+      var pat = $('#pattern-area')
+      console.log('added row')
+      $(pat).append(blankRow)
+  })
+
+  $('#rem-row').on('click', function(){
+    var pat = $('#pattern-area')
+    $(pat).children().last().remove()
+    console.log('removed row')
+  })
+
   $('#row-1-spinner').on('focusin', function(){
         $(this).attr('oldValue', this.value);
   })
@@ -11,9 +51,9 @@ $(document).ready(function() {
 
     console.log(prev)
     console.log(curr)
+    console.log(blankRow)
 
     if(dir >= -1) {
-      var blank = '                        <td><div class="box knit"></div></td>'
       switch(dir) {
         case 1:
           console.log('added')
