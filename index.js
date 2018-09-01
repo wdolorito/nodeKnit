b157a4f13aae3bc91975b5dac00f30ae80425c43
William Dolorito <wdolorito@gmail.com>
2018-09-01 13:10:13 -0700

    Added node port configuration

--- index.js
+++ index.js
@@ -2,8 +2,16 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 const server = require('http').createServer(app)
-const port = 3000
 const router = require('./routes')
+const fs = require('fs')
+let port = 3000
+
+try {
+  port = fs.readFileSync('port', 'utf8')
+  console.log('input port: ' + port)
+} catch(error) {
+  console.log(error.stack)
+}
 
 app.use(express.static(__dirname + '/static'))
 app.use('/jspdf', express.static(__dirname + '/node_modules/jspdf/dist'))
